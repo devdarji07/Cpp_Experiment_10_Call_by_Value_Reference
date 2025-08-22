bede2b56e62f48960585dad5d308cac2760f1316
Dev <devkdarji@gmail.com>
2025-08-23 00:07:47 +0530

    Create Call_By_Reference.cpp

---
+++ Call_By_Reference.cpp
@@ -0,0 +1,38 @@
+//Dev Darji
+//24070123033
+//Batch: A2
+
+#include <iostream>
+using namespace std;
+
+void swap(int *a, int *b){
+    int swap;
+    swap = *a;
+    *a=*b;
+    *b = swap;
+}
+
+int main() {
+    int x= 500;
+    int y = 400;
+    
+    cout<<"Value of original X: "<<x<<endl;
+    cout<<"Value of original Y: "<<y<<endl;
+    cout<<"\n"<<"---After swapping---"<<"\n";
+    
+    swap (&x,&y);
+    cout<<"\n"<<"Value of X: "<<x<<endl;
+    cout<<"Value of Y: "<<y<<endl;
+    
+    return 0;
+}  
+
+# Ouput:
+
+Value of original X: 500
+Value of original Y: 400
+
+---After swapping---
+
+Value of X: 400
+Value of Y: 500
